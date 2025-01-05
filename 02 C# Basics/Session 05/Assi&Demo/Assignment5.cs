4f68ff1f26e9cc46c98239793c1856ab0de115bb
ZiadMysara <ziadmysara@gmail.com>
2025-01-05 03:26:19 +0200

    Add Assignment 5 implementation with various C# functions and examples

---
+++ 02 C# Basics/Session 05/Assi&Demo/Assignment5.cs
@@ -0,0 +1,295 @@
+﻿namespace Session_05;
+
+class Assignment5
+{
+    static void Main(string[] args)
+    {
+        //P1.Run();
+        //P2.Run();
+        //P3.Run();
+        //P4.Run();
+        //P5.Run();
+        //P6.Run();
+        //P7.Run();
+        //P8.Run();
+        DemoNotes.Run();
+        
+    }
+}
+/*
+ Functions 
+ 
+1- Explain the difference between passing (Value type 
+parameters) by value and by reference then write a suitable c# 
+example.
+
+2- Explain the difference between passing (Reference type 
+parameters) by value and by reference then write a suitable c# 
+example.
+
+3- Write a c# Function that accept 4 parameters from user and 
+return result of summation and subtracting of two numbers
+
+4- Write a program in C# Sharp to create a function to calculate the sum of 
+the individual digits of a given number.
+ Output should be like 
+ Enter a number: 25 
+ The sum of the digits of the number 25 is: 7
+
+5- Create a function named "IsPrime", which receives an integer number 
+and retuns true if it is prime, or false if it is not:
+
+6- Create a function named MinMaxArray, to return the minimum and 
+maximum values stored in an array, using reference parameters
+
+7- Create an iterative (non-recursive) function to calculate the factorial 
+of the number specified as parameter
+
+8- Create a function named "ChangeChar" to modify a letter in a certain 
+position (0 based) of a string, replacing it with a different letter
+ 
+ */
+internal static class P1
+{
+    private static void swap(int a, int b)
+    {
+        int temp = a;
+        a = b;
+        b = temp;
+    }
+    private static void swapRef(ref int a,ref int b)
+    {
+        int temp = a;
+        a = b;
+        b = temp;
+    }
+    public static void Run()
+    {
+       /*
+        1- Explain the difference between passing (Value type
+        parameters) by value and by reference then write a suitable c#
+        example.
+        */
+        // by value
+        Console.WriteLine("By Value");
+        Console.WriteLine("*********");
+        int x = 10, y = 20;
+        Console.WriteLine($"Before swap x = {x} , y = {y}");
+        swap(x, y);
+        Console.WriteLine($"After swap x = {x} , y = {y}");
+       
+        // by reference
+        Console.WriteLine("By Reference");
+        Console.WriteLine("*************");
+        x = 10; y = 20;
+        Console.WriteLine($"Before swap x = {x} , y = {y}");
+        swapRef(ref x, ref y);
+        Console.WriteLine($"After swap x = {x} , y = {y}");
+        
+        // when we pass by value the original value is not changed
+        // when we pass by reference the original value is changed
+    }
+}
+internal static class P2
+{
+    private static void SumArr2(int[] arr)
+    {
+        arr = new int[] { 1, 2, 3}; // this will not change the original array
+        arr[0] = 100;
+    }
+    private static void SumArrRef2(ref int[] arr)
+    {
+        arr = new int[] { 1, 2, 3 }; // this will change the address of the original array
+        arr[0] = 4;
+    }
+    
+    public static void Run()
+    {
+        /*
+        2- Explain the difference between passing (Reference type
+        parameters) by value and by reference then write a suitable c#
+        example.
+        */
+        // by value
+        Console.WriteLine("By Value");
+        Console.WriteLine("*********");
+        int[] numbers = { 1, 2, 3 };
+        Console.WriteLine($"Before change numbers[0] = {numbers[0]}");
+        SumArr2(numbers);
+        Console.WriteLine($"After change numbers[0] = {numbers[0]}");
+        
+        // by reference
+        Console.WriteLine("By Reference");
+        Console.WriteLine("*************");
+        numbers = new int[] { 1, 2, 3 };
+        Console.WriteLine($"Before change numbers[0] = {numbers[0]}");
+        SumArrRef2(ref numbers);
+        Console.WriteLine($"After change numbers[0] = {numbers[0]}");
+       
+    }
+}
+internal static class P3
+{
+    private static void Sum(params int[] x)
+    {
+        Console.WriteLine("Summation of first two numbers: " + (x[0] + x[1]));
+    }
+    private static void Subtract(params int[] x)
+    {
+        Console.WriteLine("Subtraction of last two numbers: " + (x[2] - x[3]));
+    }
+    public static void Run()
+    {
+        /*
+        3- Write a c# Function that accept 4 parameters from user and
+        return result of summation and subtracting of two numbers
+        */
+        int a, b, c, d;
+        int []tempArr = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+        a = tempArr[0];
+        b = tempArr[1];
+        c = tempArr[2];
+        d = tempArr[3];
+        
+        Sum(a, b, c, d);
+        Subtract(a, b, c, d);
+
+    }
+}
+internal static class P4
+{
+    private static void sumOfDigites(int num)
+    {
+        int CopyNum = num;
+        int sum = 0;
+        while (num > 0)
+        {
+            sum += num % 10;
+            num /= 10;
+        }
+        Console.WriteLine($"The sum of the digits of the number {CopyNum} is: {sum}");
+    }
+    public static void Run()
+    {
+        /*
+        4- Write a program in C# Sharp to create a function to calculate the sum of
+        the individual digits of a given number.
+        Output should be like
+        Enter a number: 25
+        The sum of the digits of the number 25 is: 7    
+        */
+        Console.Write("Enter a number: ");
+        int num = int.Parse(Console.ReadLine());
+        sumOfDigites(num);
+
+        
+       
+    }
+}
+internal static class P5
+{
+    private static void IsPrime(int num)
+    {
+        bool isPrime = true;
+        for (int i = 2; i <= num / 2; i++)
+        {
+            if (num % i == 0)
+            {
+                isPrime = false;
+                break;
+            }
+        }
+        Console.WriteLine(isPrime);
+    }
+    public static void Run()
+    {
+        /*
+        5- Create a function named "IsPrime", which receives an integer number
+        and retuns true if it is prime, or false if it is not:
+        */
+        Console.Write("Enter a number: ");
+        int num = int.Parse(Console.ReadLine());
+        IsPrime(num);
+       
+    }
+}
+internal static class P6
+{
+    private static void MinMaxArray(int[] arr, ref int min, ref int max)
+    {
+        min = arr[0];
+        max = arr[0];
+        for (int i = 1; i < arr.Length; i++)
+        {
+            if (arr[i] < min)
+            {
+                min = arr[i];
+            }
+            if (arr[i] > max)
+            {
+                max = arr[i];
+            }
+        }
+        
+    }
+    public static void Run()
+    {
+        /*
+        6- Create a function named MinMaxArray, to return the minimum and
+        maximum values stored in an array, using reference parameters
+        */
+        int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        int min = 0, max = 0;
+        MinMaxArray(arr, ref min, ref max);
+        Console.WriteLine($"Min = {min} , Max = {max}");
+        
+    }
+}
+internal static class P7
+{
+    private static void Factorial(int num)
+    {
+        int fact = 1;
+        for (int i = 1; i <= num; i++)
+        {
+            fact *= i;
+        }
+        Console.WriteLine(fact);
+    }
+    public static void Run()
+    {
+        /*
+        7- Create an iterative (non-recursive) function to calculate the factorial
+        of the number specified as parameter
+        */
+        Console.Write("Enter a number: ");
+        int num = int.Parse(Console.ReadLine());
+        Factorial(num);
+       
+    }
+}
+internal static class P8
+{
+    private static void ChangeChar(ref string str, int index, char ch)
+    {
+        char[] temp = str.ToCharArray();
+        temp[index] = ch;
+        str = new string(temp);
+    }
+    public static void Run()
+    {
+        /*
+        8- Create a function named "ChangeChar" to modify a letter in a certain
+        position (0 based) of a string, replacing it with a different letter
+        */
+        Console.Write("Enter the word: ");
+        string str = Console.ReadLine();
+        Console.Write("Enter the index: ");
+        int index = int.Parse(Console.ReadLine());
+        Console.Write("Enter the character: ");
+        char ch = char.Parse(Console.ReadLine());
+        ChangeChar(ref str, index, ch);
+        Console.WriteLine(str);
+       
+    }
+}
---
+++ 02 C# Basics/Session 05/Assi&Demo/DemoNotes.cs
@@ -0,0 +1,363 @@
+﻿namespace Session_05;
+
+internal static class DemoNotes
+{
+
+    public static void Run()
+    {
+         #region casting [boxing_unboxing]
+         /* refrence type => from reference to reference */
+         //boxing casting => from value type to refrence {safe casting}/
+         //unboxing casting => from refrence type to value  {unsafe casting}/
+
+         #region Boxing
+         // // Parent Child
+         // // Dog is Animal
+         // object obj = new object();
+         // obj = "mostafa"; /*refrence type */
+         // obj = 2; /*boxing casting*/
+         //     
+         // int x = 10;
+         // object obj01 = new object();  /*boxing*/
+         // obj01 = x ;
+         //
+         #endregion
+         
+         #region Unboxing 
+         // // Child Parent
+         // // Animals is Dogs
+         // x = 10; 
+         // obj01 = new object();    
+         // // x = obj01;        /*not valid */ /*unboxing*/ 
+         //
+         // // we can fix it by typing
+         // x = (int)obj01; //but not safe 
+         #endregion
+         #endregion
+
+         
+         #region nullable data type 
+
+         #region value type 
+        //  //int x = null;  // number only 
+        //  int ? xN = null; // int number and null // this is syntax sugar 
+        //
+        //  Nullable<int> number; /*we can write as that but not usable now days */
+        // // int ? xN == Nullable<int> xN; // this is syntax sugar only
+        //   
+        //  int x = 10;
+        //  int? yN = /*(int?)*/x;
+        //
+        //  xN = null;
+        //  // int y  = xN; // error
+        //  int y = (int)xN ; // IT IS LIKE I SWEAR TO HIM THAT IT IS NOT NULL
+        //                    // BUT IF IT IS NULL IT WILL THROW AN EXCEPTION  
+        //
+        //  if (xN != null) 
+        //      y = (int)xN; // safe casting
+        //  else 
+        //      y = 0;
+        //  
+        //
+        //  y = xN.HasValue? xN.Value : 0; // xN.Value => int
+        //
+        //
+        //  Console.WriteLine(y);
+         #endregion
+
+         #region reference type
+         // string name = null;
+         //
+         // string? nameN = null; // compile inhance only use nullable  
+         //
+         // string nameF = null!; //  compile inhance only use null forgiving operator
+         //
+
+         #endregion
+
+         #endregion
+
+         #region Null propagation operator 
+         // int[] numbers = null; 
+         //
+         //
+         // for (int i = 0;numbers != null && i < numbers?.Length; i++)
+         // {
+         //     Console.WriteLine(numbers[i]);
+         // }
+         //
+         //
+         //
+         //
+         // // i < numbers?.length 
+         // /*
+         //  numbers  => object [length]
+         //  numbers => null [null]
+         // */
+         // int length = numbers?.Length ?? 0; // if numbers is null return 0
+         //
+         // Console.WriteLine(length);
+         //
+         // //Console.WriteLine(Length);
+         // Employee Emp = new Employee();
+         // Emp. Department = new Department();
+         // /*
+         // if(Emp != null)
+         // { 
+         //    if(Emp. Department != null)
+         //    {
+         //        Console.WriteLine(Emp. Department.Name);
+         //    }
+         // }
+         // */
+         //
+         // Console.WriteLine(Emp?.Department?.Name ?? "Not Found");
+         // // this what we use in the real life
+
+         #endregion
+
+         #region Functions 
+
+         // DemoNotes.PrintShape(10 , "(-)"); // passing by order
+         //
+         // DemoNotes.PrintShape(count:10 , pattern:"fkdsm"); // passing by name
+         //
+         // Console.WriteLine("hello \n ahmed "); // new line 
+         // Console.WriteLine("hello \t ahmed "); // four spaces (tap) 
+         // Console.WriteLine("hello \\ ahmed "); // print "\"  // skip meaning of "\"
+         // Console.WriteLine(@"hello \ ahmed "); // print "\"  // skip meaning of "\"
+
+         #endregion
+
+         #region value Type (int, float, double, char, bool, ...)
+         #region send by (value, refrence)
+         // // value
+         // int A = 5;
+         // int B = 10;
+         // Console.WriteLine(A);
+         // Console.WriteLine(B);
+         //
+         // DemoNotes.Swap(A , B);
+         // Console.WriteLine(A); // 5 // not changed
+         // Console.WriteLine(B); // 10 // not changed
+         
+         // // refrence
+         // A = 5;
+         // B = 10;
+         // Console.WriteLine(A);
+         // Console.WriteLine(B);
+         //
+         // DemoNotes.SwapByRef( ref A, ref B);
+         // Console.WriteLine(A); // 10 // changed
+         // Console.WriteLine(B); // 5 // changed
+         #endregion
+         #endregion
+
+
+
+         #region refrence type (array, ...)
+         #region send by (value, refrence)
+
+         #region Example 1
+         // // Value 
+         // int[] numbers = { 1, 2, 3 };
+         // Console.WriteLine(DemoNotes.SumArr(numbers)); // 105
+         // Console.WriteLine(numbers[0]); // 100
+         //
+         // // refrence
+         // Console.WriteLine(DemoNotes.SumArrRef(ref numbers)); // 105
+         // Console.WriteLine(numbers[0]); // 100
+         //
+         // // here no deference between value and reference
+         #endregion
+         
+         #region Example 2
+         // // Value 
+         // int[] numbers = { 1, 2, 3 };
+         // Console.WriteLine(DemoNotes.SumArr2(numbers)); // 15
+         // Console.WriteLine(numbers[0]); // 1
+         //
+         // // refrence
+         // Console.WriteLine(DemoNotes.SumArrRef2(ref numbers)); // 15
+         // Console.WriteLine(numbers[0]); // 4
+         //
+         // // Here we can see the deference between value and reference
+            
+         #endregion
+         #endregion
+         #endregion
+
+         #region Functions - passing by out 
+         // int A = 10; 
+         // int B = 20;
+         // int sum = 0;
+         // int mul = 0;
+         //
+         // DemoNotes.SumMul (A , B , out sum, out mul); // out (output)
+         //
+         // Console.WriteLine(sum);            /* value */
+         // Console.WriteLine(mul);
+         //
+         // A = 10; 
+         // B = 20;
+         // sum = 0;
+         // mul = 0;
+         //
+         // DemoNotes.SumMulRef (A , B , ref sum, ref mul); // ref (input and output)
+         //
+         // Console.WriteLine(sum);            /* refrence */
+         // Console.WriteLine(mul);
+
+         #endregion
+
+         #region function params 
+
+         // Console.WriteLine(DemoNotes.SumArr(1, 2, 3, 4, 5, 6, 7, 8, 9, 10 /* ,... */));
+         //
+         // int a = 12 , b = 13 , c = 14 ;
+         // Console.WriteLine("A : {0} , b: {1} , c :  {2} " , a , b, c);// build in params in WriteLine
+         //
+         //
+         #endregion
+
+        #region  Notes
+        // ? => nullable
+        // ?? => null coalescing operator
+        // ?. => null propagation operator
+        // ! => null forgiving operator
+        
+        // 15 < null => false
+        // x < null => false
+        // null in comparison with any number is always false
+        
+        // anything accepts null is you have to check it before using it
+        
+        //Console.WriteLine(Emp?.Department?.Name ?? "Not Found");
+        // this what we use in the real life
+        // ?? means if the left side is null return the right side ("Not Found")
+        
+        /*
+         * NameSpace can have
+         * 1- Classes
+         * 2- Interfaces
+         * 3- Enums
+         * 4- Delegates
+         * 5- Structs
+         */
+        
+        /*
+         * Class can have
+         * Data
+         * Functions
+         */
+        
+        /*
+         * Types of Function
+         * 1. Class Member Function [static Function]
+         * 2. object Member Function [non Static Function]
+         */
+        
+        // ref => input and output
+        // out => output only
+        // params => take a lot of values and put it in array
+        
+        // Default value given in (class, array, ...) not in main function 
+        // Nullable Check is a new feature in (.NET 6.0)
+        
+        
+        #endregion
+
+    }
+    
+    
+    public static void Swap(int x, int y)
+    {
+        int temp = x;
+        x = y;
+        y = temp;
+    }
+    public static void SwapByRef(ref int x, ref int y)
+    {
+        int temp = x;
+        x = y;
+        y = temp;
+    }
+    public static int SumArr(params int[] arr) // params take a lot of values and put it in array
+    {
+        int sum = 0;
+        arr[0] = 100;
+        for (int i = 0; i < arr.Length; i++)
+        {
+            sum += arr[i];
+        }
+        return sum;
+
+    }
+    public static int SumArrRef(ref int[] arr)
+    {
+        int sum = 0;
+        arr[0] = 100;
+        for (int i = 0; i < arr.Length; i++)
+        {
+            sum += arr[i];
+        }
+        return sum;
+
+    }
+    public static int SumArr2(params int[] arr)
+    {
+        int sum = 0;
+        arr = new int [] {4, 5, 6 };
+        for (int i = 0; i < arr.Length; i++)
+        {
+            sum += arr[i];
+        }
+        return sum;
+
+    }
+    public static int SumArrRef2(ref int[] arr)
+    {
+        int sum = 0;
+        arr = new[] { 4, 5, 6 };
+        for (int i = 0; i < arr.Length; i++)
+        {
+            sum += arr[i];
+        }
+        return sum;
+
+    }
+    public static void SumMul(int x, int y, out int sum, out int mul)
+
+    {
+        sum = x + y;
+        mul = x * y;
+    }
+    public static void SumMulRef(int x, int y, ref int sum, ref int mul)
+
+    {
+        sum = x + y;
+        mul = x * y;
+    }
+    public static void PrintShape(int count, string pattern)
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine(pattern);
+        }
+    }
+    
+}
+
+internal class Employee
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public int ID { get; set; }
+    public Department Department { get; set; }
+}
+
+internal class Department
+{
+    public string Name { get; set; }
+    
+}
